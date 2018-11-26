f35953bae4e29035bb987cd8f69ec864a91b5350
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-11-26 22:53:45 +0900

    [add]DbRepository Method

--- Core/DbRepository.php
+++ Core/DbRepository.php
@@ -1,15 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: oukas
- * Date: 2018/11/26
- * Time: 22:09
- */
-
 namespace Core;
 
 
+/**
+ * Class DbRepository
+ * @package Core
+ */
 class DbRepository
 {
+    /**
+     * @var
+     */
+    protected $con;
 
+    /**
+     * DbRepository constructor.
+     * @param $con
+     */
+    public function __construct ($con)
+    {
+        $this->registerConnection($con);
+    }
+
+    /**
+     * @param $con
+     */
+    public function registerConnection ($con)
+    {
+        $this->con = $con;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return mixed
+     */
+    public function execute (string $sql, array $params = [])
+    {
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return mixed
+     */
+    public function fetch (string $sql, array $params = [])
+    {
+        return Self::execute($sql,$params)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return mixed
+     */
+    public function fetchAll (string $sql, array $params = [])
+    {
+        return Self::execute($sql,$params)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
